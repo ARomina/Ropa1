7efae1f6d643ec6ba949d54401f7c4646bece3b9
ARomina <aldanarominabarraza@gmail.com>
2016-10-16 17:30:48 -0300

    Updated files1
    Updated files

---
+++ src/main/java/ar/edu/grupoesfera/cursospring/modelo/Categoria.java
@@ -0,0 +1,41 @@
+package ar.edu.grupoesfera.cursospring.modelo;
+
+public enum Categoria {
+
+    MUJERES,
+    HOMBRES,
+	NIÑOS;
+}
+
+/*
+package ar.edu.grupoesfera.cursospring.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Caracteristicas {
+
+	//Declaracion y creacion
+	private List<String> categoria = new ArrayList<String>();
+	
+	//Constructor
+	public Caracteristicas(){
+			
+		//Cargado de cada una de las listas
+				
+		//Categorias
+		categoria.add("MUJERES");
+		categoria.add("HOMBRES");
+		categoria.add("NIÑOS");
+		
+		//Getters y Setters
+		public List<String> getCategoria(){
+			return categoria;
+		}
+		public List<String> setCategoria(List<String> categoria) {
+			return this.categoria = categoria;
+		}
+	
+    }
+	
+}*/
